48081fe34d041c4163724029c4e93f111ad8cc47
K0O7 <piotrdulinski25@gmail.com>
2025-06-30 16:19:48 +0200

    Update DestroyHandler.cs

--- Assets/Scripts/Objects/Destroyable/DestroyHandler.cs
+++ Assets/Scripts/Objects/Destroyable/DestroyHandler.cs
@@ -1,35 +1,18 @@
-<<<<<<< Updated upstream
-=======
-using Unity.VisualScripting;
->>>>>>> Stashed changes
 using UnityEngine;
 
 public class DestroyHandler : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [SerializeField] InventoryManager inventoryManager;
     [SerializeField] private Sprite[] loot;
     private GameObject parentObject;
     [SerializeField] private int durability; //how many hits player needs to destroy
     [SerializeField] 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         parentObject = this.gameObject;
-=======
-    [SerializeField] private GameObject loot;
-    [SerializeField] private int durability; //number of hits to destroy object
-    private GameObject mainObject;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        mainObject = this.gameObject;
-        
->>>>>>> Stashed changes
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -37,7 +20,6 @@ public class DestroyHandler : MonoBehaviour
 
     public void OnHit()
     {
-<<<<<<< Updated upstream
         if (parentObject != null)
         {
             if (durability > 0)
@@ -55,21 +37,6 @@ public class DestroyHandler : MonoBehaviour
                     }
                 }
                 Object.Destroy(parentObject);
-=======
-        if (durability > 0)
-            durability--;
-        if (durability == 0)
-        {
-            if (loot != null)
-            {
-                Object.Destroy(mainObject);
-            }
-            else
-            {
-                //put loot logic here, propably after adding inventory
-                Object.Destroy(mainObject);
-
->>>>>>> Stashed changes
             }
         }
     }
