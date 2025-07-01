af5ab8ad5cb788e4213fd3dfb7ab1f6c14651e5e
Filip Krężel <45013654+Flixirr@users.noreply.github.com>
2025-07-01 17:20:08 +0200

    add menu buttons sfx + grass hit sfx

--- Assets/Sprites/Grass/DestructibleObject.cs
+++ Assets/Sprites/Grass/DestructibleObject.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using UnityEngine;
+using FMODUnity;
 
 public class DestructibleObject : MonoBehaviour
 {
     [SerializeField] private int objectHP = 1;
     [SerializeField] private GameObject hitParticles;
+    [SerializeField] private EventReference hitSound;
     private bool destroyed = false;
 
     public void TakeDamage()
@@ -12,10 +14,12 @@ public class DestructibleObject : MonoBehaviour
         if (objectHP > 0)
         {
             Instantiate(hitParticles, transform.position, Quaternion.identity);
+            SFXManager.instance.PlayOneShot(hitSound, this.transform.position);
             objectHP--;
         }
         else
         {
+            SFXManager.instance.PlayOneShot(hitSound, this.transform.position);
             Destroy(gameObject);
         }
     }
