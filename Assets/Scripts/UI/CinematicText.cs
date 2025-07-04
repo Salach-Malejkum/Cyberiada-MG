df853e5206c99d00abe8ac2d8342e1f9e63137f4
Mateusz Salach <57152223+Salach-Malejkum@users.noreply.github.com>
2025-07-04 14:14:23 +0200

    Add intro video

--- Assets/Scripts/UI/CinematicText.cs
+++ Assets/Scripts/UI/CinematicText.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class CinematicText : MonoBehaviour
 {
@@ -10,17 +11,26 @@ public class CinematicText : MonoBehaviour
     [SerializeField] private GameObject button;
     [SerializeField] private float waitTime = 5.0f;
     [SerializeField] private string sceneName;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     private int curr_line = 0;
 
     void Start()
     {
+        videoPlayer.Play();
+        videoPlayer.loopPointReached += OnVideoEnd;
         button.SetActive(false);
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        videoPlayer.gameObject.SetActive(false);
         StartCoroutine(showLine());
     }
 
     IEnumerator showLine()
     {
+        yield return new WaitForSeconds(1);
         if (curr_line < lines.Length)
         {
             lines[curr_line].Play("line_fade");
